15d1f4760cb7de1b24faac2597701bb896e86c42
monica kang <aksenwkd92@naver.com>
2025-05-02 16:14:54 +0900

    [feature] 27

--- Project21/baseball.cpp
+++ Project21/baseball.cpp
@@ -1,3 +1,14 @@
+#include "gmock/gmock.h"
+#include <stdexcept>
+#include <string>
+
+using std::string;
+using std::exception;
+using std::length_error;
+
 class Baseball {
 public:
+	void guess(const string& string) {
+		throw length_error("Must be three letters.");
+	}
 };
